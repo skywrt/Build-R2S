d3eb62807f64e367f495c12fc6cd28221ae3ebd8
skywrt <156793219+skywrt@users.noreply.github.com>
2024-04-22 23:23:21 +0800

    Update diy-part1.sh

--- R2S/diy-part1.sh
+++ R2S/diy-part1.sh
@@ -10,9 +10,8 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+sed -i "/helloworld/d" feeds.conf.default
+sed -i '$a src-git Jejz https://github.com/Jejz168/openwrt-packages' feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
